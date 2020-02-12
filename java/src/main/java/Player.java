622e427f97ff66feb79a52d577fc22b294ead791
gowtham <gowthamraj07@gmail.com>
2020-02-12 15:57:36 +0100

    Create Player class

---
+++ java/src/main/java/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    public int player1Point = 0;
+    public String player1Score = "";
+
+    public Player() {
+    }
+}
--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,9 +1,8 @@
 
 public class TennisGame2 implements TennisGame {
-    public int player1Point = 0;
+    public final Player player1 = new Player();
     public int player2Point = 0;
 
-    public String player1Score = "";
     public String player2Score = "";
 
     public String getScore() {
@@ -27,25 +26,25 @@ public class TennisGame2 implements TennisGame {
             return "Advantage player2";
         }
 
-        player1Score = constructScore(player1Point);
+        player1.player1Score = constructScore(player1.player1Point);
         player2Score = constructScore(player2Point);
-        return player1Score + "-" + player2Score;
+        return player1.player1Score + "-" + player2Score;
     }
 
     private boolean isAdvantageForPlayer2() {
-        return player2Point > player1Point && isGamePointOrAbove(player1Point);
+        return player2Point > player1.player1Point && isGamePointOrAbove(player1.player1Point);
     }
 
     private boolean isAdvantageForPlayer1() {
-        return player1Point > player2Point && isGamePointOrAbove(player2Point);
+        return player1.player1Point > player2Point && isGamePointOrAbove(player2Point);
     }
 
     private boolean isPlayer2Win() {
-        return player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2;
+        return player2Point >= 4 && player1.player1Point >= 0 && (player2Point - player1.player1Point) >= 2;
     }
 
     private boolean isPlayer1Win() {
-        return player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2;
+        return player1.player1Point >= 4 && player2Point >= 0 && (player1.player1Point - player2Point) >= 2;
     }
 
     private String constructScore(int playerPoint) {
@@ -60,10 +59,10 @@ public class TennisGame2 implements TennisGame {
     }
 
     private String constructTieScore() {
-        if (isGamePointOrAbove(player1Point)) {
+        if (isGamePointOrAbove(player1.player1Point)) {
             return "Deuce";
         }
-        return constructScore(player1Point) + "-All";
+        return constructScore(player1.player1Point) + "-All";
     }
 
     private boolean isGamePointOrAbove(int playerPoint) {
@@ -71,11 +70,11 @@ public class TennisGame2 implements TennisGame {
     }
 
     private boolean isTie() {
-        return player1Point == player2Point;
+        return player1.player1Point == player2Point;
     }
 
     public void player1Scores() {
-        player1Point++;
+        player1.player1Point++;
     }
 
     public void player2Scores() {
